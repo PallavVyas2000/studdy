913f65596da9671cad9d589d83f897399b9793e6
Pallav Vyas <pallavvyas2000@gmail.com>
2022-04-28 19:56:58 +0530

    working signup

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,22 +2,45 @@ import React, { useState } from 'react';
 import Cookies from 'universal-cookie';
 import axios from 'axios'
 import Auth_back from '../Images/Auth_back.jpg';
+import culogo from '../Images/cu-logo.png';
 
-const initialState = {fullName: '', username: '', password: '', confirmPassword: '', phoneNumber: '', avatarURL: ''}
+const cookies = new Cookies();
+const initialState = { fullName: '', username: '', password: '', confirmPassword: '', phoneNumber: '', avatarURL: '' }
 
 const Auth = () => {
   const [isSignup, setIsSignup] = useState(true);
   const [form, setForm] = useState(initialState);
+
   const handleChange = (e) => {
-    setForm({ ...form,[e.target.name]: e.target.value});
+    setForm({ ...form, [e.target.name]: e.target.value });
   }
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const { fullName, username, password, phoneNumber, avatarURL } = form;
+    const URL = 'http://localhost:5000/auth';
+    const { data: { token, userID, hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, { username, password, fullName, phoneNumber, avatarURL })
+    
+    cookies.set('token', token);
+    cookies.set('username', username);
+    cookies.set('fullName', fullName);
+    cookies.set('userID', userID);
+
+    if(isSignup) {
+    cookies.set('phoneNumber', phoneNumber);
+    cookies.set('avatarURL', avatarURL);
+    cookies.set('hashedPassword', hashedPassword);
+    }
+
+    window.location.reload();
   }
-  const switchMode = () => { setIsSignup((prevIsSignup) => !prevIsSignup)};
+
+  const switchMode = () => { setIsSignup((prevIsSignup) => !prevIsSignup) };
+
   return (
     <div className="auth__form-container">
       <div className="auth__form-container_fields">
+        <h1 className="auth-form__heading">STUDDY.</h1>
         <div className="auth__form-container_fields-content">
           <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
           <form onSubmit={handleSubmit}>
@@ -68,7 +91,8 @@ const Auth = () => {
         </div>
       </div>
       <div className="auth__form-container_image">
-        <img src={Auth_back}/>
+        <img className="auth-culogo" src={culogo} />
+        <img className="auth-back" src={Auth_back} />
       </div>
     </div>
   )
