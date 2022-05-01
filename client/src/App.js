cf4331582fd7492e860ae65da6a69e9a7554a268
Pallav Vyas <pallavvyas2000@gmail.com>
2022-05-02 02:36:46 +0530

    working signup

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,26 @@ import Cookies from 'universal-cookie';
 import { ChannelList } from 'stream-chat-react';
 import { ChannelListContainer, ChannelContainer, NavBar, Auth } from './components'
 
+const cookies = new Cookies();
 const apiKey = "huprx5j4vspv";
+const authToken = cookies.get('token');
 const client = StreamChat.getInstance(apiKey);
-const authToken = false;
+
+if (authToken) {
+  client.connectUser({
+    id: cookies.get('userID'),
+    name: cookies.get('username'),
+    image: cookies.get('avatarURL'),
+    token: cookies.get('token'),
+    fullName: cookies.get('fullName'),
+    phoneNumber: cookies.get('phoneNumber'),
+    hashedPassword: cookies.get('hashedPassword')
+  }, authToken);
+}
 
 function App() {
 
-  if(!authToken) return <Auth/>;
+  if (!authToken) return <Auth />;
   return (
     <>
       <Router>
