5d6182ba9ef308806a068aa48e4f1de3100484e2
Pallav Vyas <pallavvyas2000@gmail.com>
2022-05-09 05:12:33 +0530

    Deployment

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -18,7 +18,7 @@ const Auth = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { username, password, phoneNumber, avatarURL } = form;
-    const URL = 'http://localhost:5000/auth';
+    const URL = 'https://studdy-cu.herokuapp.com/auth';
     const { data: { token, userID, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, { username, password, fullName: form.fullName, phoneNumber, avatarURL })
     
     cookies.set('token', token);
